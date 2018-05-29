3a50cd9d07f84f198f6b873a4b0463b9894328c0
Ryan Andri <ryanandri@linuxmail.org>
2018-05-29 21:29:00 +0700

    Change colour to Dark purple with grey particles
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ff3270"
+        "value": "#919191"
       },
       "shape": {
         "type": "edge",
         "stroke": {
           "width": 0,
-          "color": "#ff3270"
+          "color": "#919191"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ff3270",
+        "color": "#919191",
         "opacity": 0.8,
         "width": 1
       },
